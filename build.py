334f3f088009855781cd9f3f6fa042f63d9cc26c
Kyle Gullion <kgullion@gmail.com>
2016-02-12 19:37:31 -0500

    fixed builds

--- build.py
+++ build.py
@@ -1,41 +1,49 @@
+#!/usr/bin/env python
 import xml.etree.ElementTree as ET
 import csv
 
 
 def main():
+    print("building", end="")
     with open('LBRBUILD') as csvfile:
         library = ET.parse('Library.lbr')
         for row in csv.reader(csvfile):
+            print(".", end=""),
             name = row[0]
-            parts = set(row[1:])
+            parts = set(s.strip() for s in row[1:])
             packages = set()
             symbols = set()
             devices = set()
             for device in library.iter('deviceset'):
-                if device.get('name') in parts:
+                if device.get('name').strip() in parts:
                     devices.add(device)
                     symbols |= set(g.get('symbol') for g in device.iter('gate'))
                     packages |= set(d.get('package') for d in device.iter('device'))
 
             out = ET.parse('Template.lbr')
-
-            root = out.getroot().iter('devicesets')
+            
+            root = next(out.getroot().iter('devicesets'))
             for device in devices:
                 root.append(device)
-
+            
+            out.write(name + '.lbr')
+            
             lib = library.getroot().iter('package')
-            root = out.getroot().iter('packages')
+            root = next(out.getroot().iter('packages'))
             for package in lib:
-                if package.get('name') in symbols:
+                if package.get('name') in packages:
                     root.append(package)
-
+            
+            out.write(name + '.lbr')
+            
             lib = library.getroot().iter('symbol')
-            root = out.getroot().iter('symbols')
+            root = next(out.getroot().iter('symbols'))
             for symbol in lib:
                 if symbol.get('name') in symbols:
                     root.append(symbol)
 
             out.write(name + '.lbr')
+    print()
 
 if __name__ == "__main__":
     main()
